0adfcb94631b73e68971d4a7fffb7517ef9a53ad
rsai-praveen <mailtome2737@gmail.com>
2026-02-08 13:31:35 +0530

    Pattern square

--- src/patterns/SquarePattern.java
+++ src/patterns/SquarePattern.java
@@ -5,7 +5,7 @@ public class SquarePattern {
 	public static void pattern(int n) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				System.out.print("@ \t");
+				System.out.print("@  ");
 			}
 			System.out.println();
 		}
