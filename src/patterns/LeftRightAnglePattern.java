4ffda639a78ec0aa6217d526534119be33080c6f
rsai-praveen <mailtome2737@gmail.com>
2026-02-08 13:27:40 +0530

    Pattern left right angle triangle

---
+++ src/patterns/LeftRightAnglePattern.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class LeftRightAnglePattern {
+
+	public static void pattern(int n)
+	{
+		for (int i=0;i<n;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("@  ");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		pattern(5);
+
+	}
+
+}
